b75134bcb619279577205abbc45aa4939163d318
rogerhuang <haoming5674@gmail.com>
2024-02-26 17:28:47 -0500

    test

--- openai_helloworld.py
+++ openai_helloworld.py
@@ -4,5 +4,5 @@ genai.configure(api_key="AIzaSyBYuDV_1MtL-E3IMxoiQyBhoA7PWPgm6x8")
 
 model = genai.GenerativeModel('gemini-pro')
 
-response = model.generate_content("Reply with 'Hello World! How are you?' ")
+response = model.generate_content("Reply with 'Hello World! How are you?'")
 print(response.text) 
