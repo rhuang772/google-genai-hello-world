55c6275b08ffd248380b4bb396012f2ac41d7262
rogerhuang <haoming5674@gmail.com>
2024-02-26 17:46:59 -0500

    added comments

--- openai_helloworld.py
+++ openai_helloworld.py
@@ -1,8 +1,11 @@
 import google.generativeai as genai
 
+# configure your api key here
 genai.configure(api_key="AIzaSyBYuDV_1MtL-E3IMxoiQyBhoA7PWPgm6x8")
 
+# initialize the model
 model = genai.GenerativeModel('gemini-pro')
 
+# ask the model to generate content
 response = model.generate_content("Reply with 'Hello World! How are you?'")
 print(response.text) 
